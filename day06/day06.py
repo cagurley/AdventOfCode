2ae24b57ac50bb3bb7c811a3cc88e5dcc29b1970
attic <atticus.gurley@gmail.com>
2021-01-02 01:02:48 -0500

    Added solution for day six, part two

--- day06/day06.py
+++ day06/day06.py
@@ -1,17 +1,35 @@
-def get_yes_total():
+def get_input():
     with open('input.txt') as file:
         inp = file.read().splitlines()
     inp.append('')
+    return inp
+
+
+def get_any_total():
+    inp = get_input()
     total = 0
     record = ''
     for line in inp:
         if line:
             record += line
         else:
-            resp = set()
-            resp.update(record)
+            resp = set(record)
             total += len(resp)
             record = ''
+    return total
+
+
+def get_all_total():
+    inp = get_input()
+    total = 0
+    group = []
+    for line in inp:
+        if line:
+            group.append(set(line))
+        else:
+            resp = set.intersection(*group)
+            total += len(resp)
+            group.clear()
     return total
 
 
@@ -19,7 +37,7 @@ if __name__ == '__main__':
     i = input("Part 1 or part 2?  ")
     ans = None
     if i == '1':
-        ans = get_yes_total()
+        ans = get_any_total()
     elif i == '2':
-        ans = None
+        ans = get_all_total()
     print(ans)
