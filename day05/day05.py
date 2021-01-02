f65a5e96efb6d53ed06f2395c082dd9bd4c06d2e
attic <atticus.gurley@gmail.com>
2021-01-02 00:33:02 -0500

    Added solution for day six, part one

--- day05/day05.py
+++ day05/day05.py
@@ -27,13 +27,26 @@ def get_id(string):
 
 
 def find_max_id():
-    inp = get_input()
     max_id = -1
-    for string in inp:
+    for string in get_input():
         sid = get_id(string)
         if sid > max_id:
             max_id = sid
     return max_id
+
+
+def find_missing_id():
+    ids = []
+    for string in get_input():
+        ids.append(get_id(string))
+    ids.sort()
+    lower = 1
+    part = (len(ids) - 1) // 2
+    while part > 0:
+        if ids[lower + part] == ids[lower] + part:
+            lower += part + 1
+        part //= 2
+    return ids[lower] + 1
 
 
 if __name__ == '__main__':
@@ -42,5 +55,5 @@ if __name__ == '__main__':
     if i == '1':
         ans = find_max_id()
     elif i == '2':
-        ans = None
+        ans = find_missing_id()
     print(ans)
