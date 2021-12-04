887e51b4c0e9c5d266ff4b2801e88e86de122ad5
attic <atticus.gurley@gmail.com>
2021-12-04 16:31:42 -0500

    Added solution for 2021, day two, part two

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -4,7 +4,7 @@ def get_input():
     return inp
 
 
-def get_course_product():
+def get_simple_product():
     inp = get_input()
     fwd = 0
     depth = 0
@@ -22,11 +22,31 @@ def get_course_product():
     return fwd * depth
 
 
+def get_complex_product():
+    inp = get_input()
+    fwd = 0
+    depth = 0
+    aim = 0
+    for _ in inp:
+        cmd, num = _.split()
+        num = int(num)
+        if cmd[0] == 'd':
+            aim += num
+        elif cmd[0] == 'u':
+            aim -= num
+        else:
+            fwd += num
+            depth += aim*num
+            if depth < 0:
+                depth = 0
+    return fwd * depth
+
+
 if __name__ == '__main__':
     i = input("Part 1 or part 2?  ")
     ans = None
     if i == '1':
-        ans = get_course_product()
+        ans = get_simple_product()
     elif i == '2':
-        ans = None
+        ans = get_complex_product()
     print(ans)
