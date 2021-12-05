ab955f4d353943758f878af22807dba4954d22df
attic <atticus.gurley@gmail.com>
2021-12-04 21:25:41 -0500

    Added solution for 2021, day four, part one

---
+++ 2021/day04/day04.py
@@ -0,0 +1,62 @@
+class Board:
+    def __init__(self):
+        self.tiles = {}
+        self.state = [[False for _ in range(5)] for _ in range(5)]
+
+    def check(self, val):
+        if val in self.tiles:
+            x, y = self.tiles[val]
+            self.state[x][y] = True
+            for _ in range(5):
+                if self.state[_][y] is False:
+                    break
+                elif _ == 4:
+                    return True
+            for _ in range(5):
+                if self.state[x][_] is False:
+                    break
+                elif _ == 4:
+                    return True
+        return False
+
+    def calculate_score(self, val):
+        sum = 0
+        for k, v in self.tiles.items():
+            x, y = v
+            if self.state[x][y] is False:
+                sum += int(k)
+        return sum * int(val)
+
+
+def get_input():
+    with open('input.txt') as file:
+        calls = file.readline().split(',')
+        boards = []
+        while file.readline():
+            b = file.read(75)
+            board = Board()
+            for x, line in enumerate(b.splitlines()):
+                for y, _ in enumerate(line.split()):
+                    board.tiles.update({_: (x, y)})
+            boards.append(board)
+    return calls, boards
+
+
+def get_winning_score():
+    calls, boards = get_input()
+    winner = None
+    for c in calls:
+        for n, b in enumerate(boards):
+            if b.check(c):
+                return b.calculate_score(c)
+    return None
+
+
+if __name__ == '__main__':
+    i = input("Part 1 or part 2?  ")
+    ans = None
+    if i == '1':
+        ans = get_winning_score()
+    elif i == '2':
+        ans = None
+    print(ans)
