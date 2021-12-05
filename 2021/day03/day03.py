150f8cedbee336206c9834683405c042888f9c5e
attic <atticus.gurley@gmail.com>
2021-12-04 19:14:08 -0500

    Added solution for 2021, day three, part two

--- 2021/day03/day03.py
+++ 2021/day03/day03.py
@@ -24,11 +24,52 @@ def get_power_consumption():
     return gamma * epsilon
 
 
+def get_life_support_rating():
+    inp = get_input()
+    inp.sort()
+    nlen = len(inp[0])
+    rlen = len(inp)
+
+    a = 0
+    b = rlen - 1
+    for n in range(nlen):
+        if a == b:
+            break
+        m = a + 1
+        while m <= b and inp[m][n] == inp[m-1][n]:
+            m += 1
+        if m > b:
+            pass
+        elif (m - a) > (b + 1 - m):
+            b = m - 1
+        else:
+            a = m
+    oxy = int(inp[b], 2)
+
+    a = 0
+    b = rlen - 1
+    for n in range(nlen):
+        if a == b:
+            break
+        m = a + 1
+        while m <= b and inp[m][n] == inp[m-1][n]:
+            m += 1
+        if m > b:
+            pass
+        elif (m - a) > (b + 1 - m):
+            a = m
+        else:
+            b = m - 1
+    co2 = int(inp[a], 2)
+
+    return oxy * co2
+
+
 if __name__ == '__main__':
     i = input("Part 1 or part 2?  ")
     ans = None
     if i == '1':
         ans = get_power_consumption()
     elif i == '2':
-        ans = None
+        ans = get_life_support_rating()
     print(ans)
