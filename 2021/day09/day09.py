28f34e277d03281ba09421a3b6b4df919a2c65b3
attic <atticus.gurley@gmail.com>
2021-12-09 02:00:46 -0500

    Added solution for 2021, day nine, part two

--- 2021/day09/day09.py
+++ 2021/day09/day09.py
@@ -28,11 +28,55 @@ def get_low_point_risk_sum():
     return sum
 
 
+def get_largest_basin_product():
+    inp = get_input()
+    h = len(inp)
+    w = len(inp[0])
+    basins = []
+    for y in range(h):
+        row = []
+        basin = {y: set()}
+        for x in range(w):
+            if inp[y][x] < 9:
+                basin[y].add(x)
+            else:
+                if basin[y]:
+                    row.append(basin.copy())
+                basin = {y: set()}
+        for r in row:
+            joins = []
+            for n, b in enumerate(basins):
+                if y-1 in b:
+                    if b[y-1] & r[y]:
+                        joins.append(n)
+            if joins:
+                existing = [basins[_] for _ in joins]
+                joins.reverse()
+                for _ in joins:
+                    basins.pop(_)
+                for _ in existing:
+                    for k, v in _.items():
+                        if k in r:
+                            r[k] |= v
+                        else:
+                            r[k] = v
+            basins.append(r)
+    sizes = []
+    for basin in basins:
+        size = 0
+        for v in basin.values():
+            size += len(v)
+        sizes.append(size)
+    sizes.sort()
+    return sizes[-3] * sizes[-2] * sizes[-1]
+
+
+
 if __name__ == '__main__':
     ipt = input("Part 1 or part 2?  ")
     ans = None
     if ipt == '1':
         ans = get_low_point_risk_sum()
     elif ipt == '2':
-        ans = None
+        ans = get_largest_basin_product()
     print(ans)
