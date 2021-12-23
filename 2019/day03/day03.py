a07f1e33b67a1208ebefd14ba2ec4ad695141a2a
attic <atticus.gurley@gmail.com>
2021-12-22 20:42:45 -0500

    Added solution for 2019, day three, part two

--- 2019/day03/day03.py
+++ 2019/day03/day03.py
@@ -34,11 +34,43 @@ def get_nearest_intersection_md():
     return None
 
 
+def get_min_intersection_sd():
+    inp = get_input()
+    wires = [{}, {}]
+    min_delay = None
+    for i, w in enumerate(inp):
+        curr = (0, 0)
+        delay = 0
+        for d in w:
+            letter, num = d[0], int(d[1:])
+            inc = 1
+            if letter in ('L', 'D'):
+                inc = -1
+            if letter in ('R', 'L'):
+                for _ in range(num):
+                    delay += 1
+                    curr = (curr[0] + inc, curr[1])
+                    if curr not in wires[i]:
+                        wires[i][curr] = delay
+            else:
+                for _ in range(num):
+                    delay += 1
+                    curr = (curr[0], curr[1] + inc)
+                    if curr not in wires[i]:
+                        wires[i][curr] = delay
+    for key, val in wires[0].items():
+        if key in wires[1]:
+            delay = val + wires[1][key]
+            if min_delay is None or min_delay > delay:
+                min_delay = delay
+    return min_delay
+
+
 if __name__ == '__main__':
     ipt = input("Part 1 or part 2?  ")
     ans = None
     if ipt == '1':
         ans = get_nearest_intersection_md()
     elif ipt == '2':
-        ans = None
+        ans = get_min_intersection_sd()
     print(ans)
