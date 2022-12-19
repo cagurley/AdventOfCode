eb5ad5d2f9a7083b22f7b48c8e1dedf037d0d1a7
attic <atticus.gurley@gmail.com>
2022-12-19 15:16:04 -0500

    Added 2022 directory and solution for 2022, day one, part one

---
+++ 2022/day01/day01.py
@@ -0,0 +1,30 @@
+def get_input():
+    with open('input.txt') as file:
+        inp = file.read().splitlines()
+        inp.append('')
+    return inp
+
+
+def get_max_total():
+    inp = get_input()
+    max_total = 0
+    i = 0
+    while i < len(inp):
+        total = 0
+        while inp[i]:
+            total += int(inp[i])
+            i += 1
+        if max_total < total:
+            max_total = total
+        i += 1
+    return max_total
+
+
+if __name__ == '__main__':
+    ipt = input("Part 1 or part 2?  ")
+    ans = None
+    if ipt == '1':
+        ans = get_max_total()
+    elif ipt == '2':
+        ans = None
+    print(ans)
