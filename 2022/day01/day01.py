98fd9b831a627f521b273667096711b0565ec4be
attic <atticus.gurley@gmail.com>
2022-12-19 15:19:36 -0500

    Added solution for 2022, day one, part two

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -20,11 +20,26 @@ def get_max_total():
     return max_total
 
 
+def get_top3_total():
+    inp = get_input()
+    totals = []
+    i = 0
+    while i < len(inp):
+        total = 0
+        while inp[i]:
+            total += int(inp[i])
+            i += 1
+        totals.append(total)
+        i += 1
+    totals.sort()
+    return sum(totals[-3:])
+
+
 if __name__ == '__main__':
     ipt = input("Part 1 or part 2?  ")
     ans = None
     if ipt == '1':
         ans = get_max_total()
     elif ipt == '2':
-        ans = None
+        ans = get_top3_total()
     print(ans)
