73529477c9952722d732015c0bfa0370597b8678
attic <atticus.gurley@gmail.com>
2024-01-07 21:50:38 -0500

    Added solution for 2023, day two, part two

--- 2023/day02/day02.py
+++ 2023/day02/day02.py
@@ -30,11 +30,31 @@ def get_possible_sum():
     return total
 
 
+def get_minimum_product_sum():
+    total = 0
+    for line in get_input():
+        _, reveals = line.split(': ')
+        reveals = reveals.split('; ')
+        red, green, blue = 0, 0, 0
+        for r in reveals:
+            for show in r.split(', '):
+                num, color = show.split(' ')
+                num = int(num)
+                if color == 'red' and num > red:
+                    red = num
+                elif color == 'green' and num > green:
+                    green = num
+                elif color == 'blue' and num > blue:
+                    blue = num
+        total += red * green * blue
+    return total
+
+
 if __name__ == '__main__':
     ipt = input("Part 1 or part 2?  ")
     ans = None
     if ipt == '1':
         ans = get_possible_sum()
     elif ipt == '2':
-        ans = None
+        ans = get_minimum_product_sum()
     print(ans)
