4e7b0936100df48a0831b46ad5ee4f3afc3d090c
attic <atticus.gurley@gmail.com>
2024-10-23 00:00:04 -0400

    Added solution for 2023, day four, part two

--- 2023/day04/day04.py
+++ 2023/day04/day04.py
@@ -24,11 +24,33 @@ def get_point_total():
     return points
 
 
+def get_instance_count():
+    inp = get_input()
+    cards = []
+    for line in inp:
+        winning, given = line.split(': ')[1].split(' | ')
+        winning = re.findall(r'\d+', winning)
+        given = re.findall(r'\d+', given)
+        win_count = 0
+        for num in given:
+            if num in winning:
+                win_count += 1
+        cards.append([1, win_count])
+
+    card_count = 0
+    for i, card in enumerate(cards):
+        card_count += card[0]
+        if card[1] > 0:
+            for n in range(i+1, card[1]+i+1):
+                cards[n][0] += card[0]
+    return card_count
+
+
 if __name__ == '__main__':
     ipt = input("Part 1 or part 2?  ")
     ans = None
     if ipt == '1':
         ans = get_point_total()
     elif ipt == '2':
-        ans = None
+        ans = get_instance_count()
     print(ans)
