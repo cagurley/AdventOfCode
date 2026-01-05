5645a465f3a66eceb2da961ec070f6d5d3784eb1
attic <atticus.gurley@gmail.com>
2026-01-05 03:13:52 -0500

    Added solution for 2025, day one, part two

--- 2025/day01/day01.py
+++ 2025/day01/day01.py
@@ -4,7 +4,7 @@ def get_input():
     return inp
 
 
-def get_password():
+def get_stop_password():
     count = 0
     position = 50
     for line in get_input():
@@ -17,11 +17,34 @@ def get_password():
     return count
 
 
+def get_rotate_password():
+    count = 0
+    position = 50
+    for line in get_input():
+        clicks = int(line[1:])
+        while True:
+            if clicks > 99:
+                count += 1
+                clicks -= 100
+            else:
+                if line[0] == 'L':
+                    clicks = -clicks
+                if position == 0:
+                    position += clicks
+                else:
+                    position += clicks
+                    if not (0 < position < 100):
+                        count += 1
+                position %= 100
+                break
+    return count
+
+
 if __name__ == '__main__':
     ipt = input("Part 1 or part 2?  ")
     ans = None
     if ipt == '1':
-        ans = get_password()
+        ans = get_stop_password()
     elif ipt == '2':
-        ans = None
+        ans = get_rotate_password()
     print(ans)
