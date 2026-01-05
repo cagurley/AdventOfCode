e460aa96d167451d5334d47169e44bccbbde5a0d
attic <atticus.gurley@gmail.com>
2026-01-05 05:32:18 -0500

    Added solution for 2025, day three, part two

--- 2025/day03/day03.py
+++ 2025/day03/day03.py
@@ -4,7 +4,7 @@ def get_input():
     return inp
 
 
-def get_total_output_joltage():
+def get_total_output_joltage_2():
     total = 0
     for bank in get_input():
         for i in range(9, 0, -1):
@@ -18,11 +18,29 @@ def get_total_output_joltage():
     return total
 
 
+def get_total_output_joltage_12():
+    total = 0
+    for bank in get_input():
+        joltage = ""
+        start = 0
+        offset = 11
+        while offset > -1:
+            for i in range(9, 0, -1):
+                index = bank[start:len(bank)-offset].find(str(i))
+                if index > -1:
+                    joltage += str(i)
+                    start += index + 1
+                    offset -= 1
+                    break
+        total += int(joltage)
+    return total
+
+
 if __name__ == '__main__':
     ipt = input("Part 1 or part 2?  ")
     ans = None
     if ipt == '1':
-        ans = get_total_output_joltage()
+        ans = get_total_output_joltage_2()
     elif ipt == '2':
-        ans = None
+        ans = get_total_output_joltage_12()
     print(ans)
