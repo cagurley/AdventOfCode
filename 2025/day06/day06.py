a3a27a62e6b8464f5680ff63aa338c7fb255eeb1
attic <atticus.gurley@gmail.com>
2026-01-07 18:12:13 -0500

    Added solution for 2025, day six, part two

--- 2025/day06/day06.py
+++ 2025/day06/day06.py
@@ -1,11 +1,13 @@
 def get_input():
     with open('input.txt') as file:
-        inp = [line.split() for line in file]
+        inp = file.read().splitlines()
     return inp[:-1], inp[-1]
 
 
-def get_answer_total():
+def get_horizontal_total():
     terms, ops = get_input()
+    terms = [line.split() for line in terms]
+    ops = ops.split()
     total = 0
     i = 0
     length = len(ops)
@@ -24,11 +26,43 @@ def get_answer_total():
     return total
 
 
+def get_vertical_total():
+    lines, ops = get_input()
+    total = 0
+    stops = []
+    for i, c in enumerate(ops):
+        if c != " ":
+            stops.append(i)
+
+    i = len(ops) - 1
+    stops.reverse()
+    for stop in stops:
+        terms = []
+        while i >= stop:
+            term = ""
+            for line in lines:
+                term += line[i]
+            terms.append(int(term))
+            i -= 1
+        if ops[stop] == "+":
+            answer = 0
+            for term in terms:
+                answer += term
+            total += answer
+        else:
+            answer = 1
+            for term in terms:
+                answer *= term
+            total += answer
+        i -= 1
+    return total
+
+
 if __name__ == '__main__':
     ipt = input("Part 1 or part 2?  ")
     ans = None
     if ipt == '1':
-        ans = get_answer_total()
+        ans = get_horizontal_total()
     elif ipt == '2':
-        ans = None
+        ans = get_vertical_total()
     print(ans)
